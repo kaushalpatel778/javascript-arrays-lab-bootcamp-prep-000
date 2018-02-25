2754c6294f5fe3e3a8de307766698e0719040cfe
Kaushal Patel <kaushalpatel778@gmail.com>
2018-02-25 17:44:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newKittens = [...kittens];
-  newKittens.push(name);
-  return newKittens;
+  var newArray = [...kittens];
+  newArray.push(name);
+  return newArray;
 }
 /*
 function appendKitten(name){
-  var newKittens = [...kittens]
-  newKittens.push(name)
-  return newKittens
+  var newArray = [...kittens]
+  newArray.push(name)
+  return newArray
 }*/
